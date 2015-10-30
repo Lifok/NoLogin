a8bfa7aabde16254b283d25c76c542908ccb5f73
Lifok <masse.g.noe@gmail.com>
2015-10-31 00:36:08 +0100

    Account changes

--- src/main/java/fr/utopics/nologin/account/Account.java
+++ src/main/java/fr/utopics/nologin/account/Account.java
@@ -2,41 +2,41 @@ package fr.utopics.nologin.account;
 
 public class Account
 {
-    private String uuid, displayName, accessToken, userId, username;
-    
-    public Account(String uuid, String displayName, String accessToken, String userId, String username)
-    {
-    	this.uuid = uuid;
-    	this.displayName = displayName;
-    	this.accessToken = accessToken;
-    	this.userId = userId;
-    	this.username = username;
-    }
-    
-    public String getUUID()
-    {
-    	return uuid;
-    }
-    
-    public String getDisplayName() 
-    {
-    	return displayName;
-    }
-    
-    public String getAccessToken() 
-    {
-    	return accessToken;
-    }
-    
-    public String getUserId()
-    {
-    	return userId;
-    }
-    
-    public String getUsername()
-    {
-    	return username;
-    }
+	private String uuid, displayName, accessToken, userId, username;
+
+	public Account(String uuid, String displayName, String accessToken, String userId, String username)
+	{
+		this.uuid = uuid;
+		this.displayName = displayName;
+		this.accessToken = accessToken;
+		this.userId = userId;
+		this.username = username;
+	}
+
+	public String getUUID()
+	{
+		return uuid;
+	}
+
+	public String getDisplayName() 
+	{
+		return displayName;
+	}
+
+	public String getAccessToken() 
+	{
+		return accessToken;
+	}
+
+	public String getUserId()
+	{
+		return userId;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
 
 	public void setAccessToken(String accessToken) {
 		this.accessToken = accessToken;
